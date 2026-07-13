851842f04396a6708732b7ded43796b794e20efa
zyb <1940988349@qq.com>
2026-07-13 11:59:01 +0800

    feat: 修改配置

--- src/naive-proxy/config.ts
+++ src/naive-proxy/config.ts
@@ -2,4 +2,4 @@ export const urls = [
   "https://www.gitlabip.xyz/Alvin9999/PAC/master/backup/img/1/2/ipp/naiveproxy/1/config.json",
   "https://www.gitlabip.xyz/Alvin9999/PAC/master/backup/img/1/2/ipp/naiveproxy/2/config.json",
 ];
-export const proxyAddress = "http://127.0.0.1:34567";
+export const proxyAddress = "http://0.0.0.0:34567";
